8dbea991cbfc72b93e5af8b7ae692c1c74d144f0
Dmitri So <dmitriso@yahoo.com>
2020-12-05 17:19:47 -0500

    Created event for save button.

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,44 @@
 // DOM Variables
-var currentDayEl = document.getElementById("currentDay");
+var currentDayEl = $("#currentDay");
+var todoInput = $(".task");
 var date = moment().format("dddd, MMM. Do YY");
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
+var todos = [];
+
+
 // THEN the current day is displayed at the top of the calendar
 // Must have funtion that uses the current New date and displays on heading
 $(document).ready(function() {
     $("#currentDay").text(date);
 })
 
+// Retrieve stored todos from local storage
+function init() {
+    // Parses JSON string to an object
+    var storedTodos = JSON.parse(localStorage.getItem("todos"));
+    console.log(storedTodos)
+}
 
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// Must have a function changes block status through style attribute color coded for past,present, and future
+// Stores user todo tasks
+function storeTodos() {
+    // Stringify an set "todos" key in localStorage to todos
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
 
+// This allows user to save tasks to localStorage when the button is clicked
+$(".saveBtn").on("click",function(event) {
+    event.preventDefault();
+    var todoText = todoInput.val();
+    // Return if user leaves field empty
+    if (todoText === "") {
+        return;
+    }
+    // Sets value of "todoText" to "todos"
+    todos.push(todoText);
+    console.log(todoText);
+
+storeTodos();
+init();
+})
 
 // WHEN I click into a time block
 // THEN I can enter an event
